a81566d42f042abbaaf967424ca9964fbcfce074
Nojipiz <nojipiz@protonmail.com>
2020-05-27 15:38:57 -0500

    Added calculateSize

--- src/models/strips/HookStrip.java
+++ src/models/strips/HookStrip.java
@@ -14,9 +14,17 @@ public class HookStrip extends Strip{
         this.diameter = diameter;
         this.sizes = sizes;
         this.hook = hook;
+        calculateSize();
     }
 
     public void calculateSize(){
-
+        int indexSize = sizes.indexOf("/");
+        String tempDiameter = sizes.substring(0, indexSize);
+        String tempOver = sizes.substring(indexSize+1);
+        double diameter = Double.parseDouble(Strip.correctNumber(tempDiameter));
+        double overlap = Double.parseDouble(Strip.correctNumber(tempOver));
+        double doubHook = Double.parseDouble(Strip.correctNumber(hook.replaceAll("G", "")));
+        double tempSize = (2 * Math.PI * (diameter / 2) + overlap + (doubHook*2));
+        sizeInMeters = Strip.roundDoubles(tempSize, 2);
     }
 }
--- src/models/strips/RoundStrip.java
+++ src/models/strips/RoundStrip.java
@@ -14,9 +14,18 @@ public class RoundStrip extends Strip{
         this.diameter = diameter;
         this.size = size;
         this.hook = hook;
+        calculateSize();
     }
 
     public void calculateSize(){
-
+        int indexSize = size.indexOf("/");
+        String turns = size.substring(0, indexSize);
+        String diameter = size.substring(indexSize+1);
+        double doubTurns = Double.parseDouble(Strip.correctNumber(turns));
+        double doubDiameter = Double.parseDouble(Strip.correctNumber(diameter));
+        double doubHook = Double.parseDouble(Strip.correctNumber(hook.replaceAll("G", "")));
+        double tempSize = (2 * Math.PI * (doubDiameter / 2) * doubTurns);
+        tempSize += (doubHook * 2);
+        sizeInMeters = Strip.roundDoubles(tempSize, 2);
     }
 }
